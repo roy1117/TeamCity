bb87abf23ff50c7620655f3c2c9d5656553fda63
roy1117 <roy.choi@edwardsvacuum.com>
2022-11-22 18:15:42 +0900

    Edited a file

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import time
+import os
+
 print('Hello world')
+directory = "build"
+parent_dir = os.path.dirname(os.path.realpath(__file__))
+path = os.path.join(parent_dir, directory)
+os.mkdir(path)
+os.chdir(path)
 with open('readMe.txt', 'w') as f:
     f.write('readme')
 print('finish..')
