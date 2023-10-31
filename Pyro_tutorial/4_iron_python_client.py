26f6bd00c1f677c2052eec4a1af7f9b504dba5e1
roy1117 <roy.choi@edwardsvacuum.com>
2023-10-31 12:08:04 +0900

    Edited Iron python scripts

--- Pyro_tutorial/4_iron_python_client.py
+++ Pyro_tutorial/4_iron_python_client.py
@@ -5,3 +5,4 @@ name = input("What is your name? ").strip()
 
 greeting_maker = Pyro4.Proxy("PYRONAME:example")    # use name server object lookup uri shortcut
 print(greeting_maker.get_fortune(name))
+greeting_maker.open_project()
--- Pyro_tutorial/4_iron_python_server.py
+++ Pyro_tutorial/4_iron_python_server.py
@@ -1,5 +1,7 @@
 import Pyro4
+import Pyro4.naming
 import threading
+import time
 import scriptengine
 
 @Pyro4.expose
@@ -8,13 +10,19 @@ class GreetingMaker(object):
         return "Hello, {0}. Here is your fortune message:\n" \
                "Behold the warranty -- the bold print giveth and the fine print taketh away.".format(name)
 
-    def close_project(self):
+    def open_project(self):
+        # project = projects.open(r"C:\Users\a00533064\OneDrive - ONEVIRTUALOFFICE\Desktop\CodeSys\Regis\Project\RegisNPIBuild.project")
+        print('Hello world')
 
 
 daemon = Pyro4.Daemon()     # make a Pyro daemon
 uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
+print('Start name server')
+th = threading.Thread(target=Pyro4.naming.startNSloop)
+th.start()
+print('Name server started')
 ns = Pyro4.locateNS()
 ns.register("example", uri)
 print("Ready. Object uri =", uri)      # print the uri so we can use it in the client later
-th = threading.Thread(target=daemon.requestLoop)
-th.start()
+daemon.requestLoop()
+
