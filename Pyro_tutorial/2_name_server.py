d02b75b5da4a872cd24d32113632f67ea8bf7f5f
roy1117 <roy.choi@edwardsvacuum.com>
2023-10-31 11:32:39 +0900

    added naming server start part

--- Pyro_tutorial/2_name_server.py
+++ Pyro_tutorial/2_name_server.py
@@ -1,5 +1,7 @@
 # saved as greeting-server.py
 import Pyro4
+import Pyro4.naming
+import threading
 
 @Pyro4.expose
 class GreetingMaker(object):
@@ -8,6 +10,12 @@ class GreetingMaker(object):
                "Tomorrow's lucky number is 12345678.".format(name)
 
 daemon = Pyro4.Daemon()                # make a Pyro daemon
+
+print('Start name server')
+th = threading.Thread(target=Pyro4.naming.startNSloop)
+th.start()
+print('Name server started')
+
 ns = Pyro4.locateNS()                  # find the name server
 uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
 ns.register("example.greeting", uri)   # register the object with a name in the name server
